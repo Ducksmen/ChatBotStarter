5e0d8de789d350014594c5c4b67f3ada409b6c7a
Kye <kdavis6911@bths.edu>
2018-10-16 10:12:37 -0400

    It's got my chatbot now again

--- src/ChatBot2.java
+++ src/ChatBot2.java
@@ -41,7 +41,7 @@ public class ChatBot2
 	 */
 	public String getGreeting()
 	{
-		return "Hi, what is up?";
+		return "Yo, what's good?";
 	}
 
 	/**
@@ -55,9 +55,14 @@ public class ChatBot2
 	{
 		String response = "";
 
-		if (trackLyrics(statement))
+		if (trackLyricsBE(statement))
 		{
 			response = transformbutterflyEffect(statement);
+		}
+
+		if (trackLyricsTTS(statement))
+		{
+			response = transformtouchTheSky(statement);
 		}
 
 		else if (statement.length() == 0)
@@ -180,6 +185,21 @@ public class ChatBot2
 		}
 		return null ;
 	}
+    private String transformtouchTheSky(String statement)
+    {
+        for (int i = 0; i < touchTheSky.length; i++)
+        {
+            if (touchTheSky[i].equals(statement))
+            {
+                if (i == touchTheSky.length - 1)
+                {
+                    return "Aight aight that's all I remember I'm sorry";
+                }
+                return touchTheSky[i + 1];
+            }
+        }
+        return null ;
+    }
 
 
 
@@ -281,7 +301,7 @@ public class ChatBot2
 		return randomHappyResponses [r.nextInt(randomHappyResponses.length)];
 	}
 
-	private boolean trackLyrics(String statement)
+	private boolean trackLyricsBE(String statement)
 	{
 		for (int i = 0; i < butterflyEffect.length; i++)
 		{
@@ -292,11 +312,22 @@ public class ChatBot2
 		}
 		return false;
 	}
+	private boolean trackLyricsTTS(String statement)
+	{
+		for (int i = 0; i < touchTheSky.length; i++)
+		{
+			if (touchTheSky[i].equals(statement))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 
 	private String [] randomNeutralResponses = {"Yo that sounds valid, tell me more",
 			"Das tuff",
 			"Are you for real?",
-			"Yo you remember the lyrics to ?",
+			"Yo you remember the lyrics to by ?",
 			"Don't trip man, iss all good",
 			"Yo you tryna bump some ?",
 			"Yo lets get back to hip hop aight?"
@@ -306,19 +337,54 @@ public class ChatBot2
 	private String [] butterflyEffect = {"For this life I cannot change",
 			"Hidden hills, deep off in the main",
 			"M&M's, sweet like candy canes",
-			"Drop the top, pop it, let it bang",
-			"Drop the top, play hide and seek",
+			"Drop the top, pop it let it bang",
+			"Drop the top play hide and seek",
 			"Jump inside, jump straight to the league",
 			"Take a sip, feel just how I be",
-			"On freeway, but no, ain't nothin' free",
-			"Bend laws, bend lanes",
-			"Been bustin' bills, but still, ain't nothin' change",
-			"You in the mob soon as you rock the chain",
+			"On freeway, but no ain't nothin' free",
+			"Bend laws bend lanes",
+			"Been bustin' bills, but still ain't nothin' change",
+			"You in the mob, soon as you rock the chain",
 			"She caught the waves just thumbin' through my braids",
-			"Heatin' up, baby, I'm just heatin' up",
-			"Need ya love, not a need it is a must",
-			"Feelin' stuck, you know how to keep me up",
-			"Icy love, icy like a hockey puck",
+			"Heatin' up, baby I'm just heatin' up",
+			"Need ya love not a need it is a must",
+			"Feelin' stuck you know how to keep me up",
+			"Icy love icy like a hockey puck"
 	};
+	private String [] touchTheSky = {"I gotta testify",
+            "Come up in the spot lookin' extra fly",
+            "Before the day I die, I'ma touch the sky",
+            "Gotta testify",
+            "Come up in the spot lookin' extra fly",
+            "Before the day I die, I'ma touch the sky",
+            "Back when they thought pink Polos would hurt the Roc",
+            "Before Cam got the stuff to pop",
+            "The doors was closed",
+            "I felt like Bad Boy's street team, I couldn't work the locks",
+            "Now let's go, take 'em back to the plan",
+            "Me and my momma hopped in that U-Haul van",
+            "Any pessimists I ain't talk to them",
+            "Plus I ain't have no phone in my apart-a-ment",
+            "Let's take 'em back to the club",
+            "Least about an hour I stand on line",
+            "I just wanted to dance, I went to Jacob an hour",
+            "After I got my advance, I just wanted to shine",
+            "Jay favorite line, Dawg, in due time!",
+            "Now he look at me, like, Dang, dawg! You where I am!",
+            "A hip-hop legend",
+            "I think I died in that accident, ‘cause this must be Heaven"};
+
+	private String [] itAintHardToTell = {"It ain't hard to tell, I excel, then prevail",
+    "The mic is contacted, I attract clientele",
+    "My mic check is life or death, breathin' a sniper's breath",
+    "I exhale the yellow smoke of buddha through righteous steps",
+    "Deep like The Shining, sparkle like a diamond",
+    "Sneak a Uzi on the island in my army jacket linin'",
+    "Hit the Earth like a comet—invasion!",
+    "Nas is like the Afrocentric Asian: half-man, half-amazin'",
+    "‘Cause in my physical I can express through song",
+    "Delete stress like Motrin, then extend strong",
+    "I drink Moët with Medusa, give her shotguns in Hell",
+    "From the spliff that I lift and inhale; it ain't hard to tell",};
 }
 
